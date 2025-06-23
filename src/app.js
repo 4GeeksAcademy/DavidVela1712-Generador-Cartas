a269811a9aaed99a43364a95d216dc9fb293e3bf
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-06-23 19:30:44 +0000

    230625-FinProyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let simbolos = ["♦","♥", "♠", "♣"];
+  let simbRandom = simbolos[randomNum(simbolos.length)];
+  let numRandom = randomNum(13)+1;
+
+  document.getElementById("p-top").textContent = simbRandom;
+  document.getElementById("p-center").textContent = numRandom;
+  document.getElementById("p-bottom").textContent = simbRandom;
+
+  if(simbRandom == "♦" || simbRandom == "♥"){
+    document.getElementById("p-top").style.color = "red";
+  document.getElementById("p-bottom").style.color = "red";
+  }
 };
+
+function randomNum(num){
+  return Math.floor(Math.random() * num);
+}
