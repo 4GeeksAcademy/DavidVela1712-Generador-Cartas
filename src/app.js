2bcdb7639904f525c13a4284a40b0fdfba0e1642
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-06-27 16:59:05 +0000

    270625-Cambio-Num-Letras

--- src/app.js
+++ src/app.js
@@ -9,9 +9,27 @@ window.onload = function() {
   let simbolos = ["♦","♥", "♠", "♣"];
   let simbRandom = simbolos[randomNum(simbolos.length)];
   let numRandom = randomNum(13)+1;
+  let carta;
+
+  switch (numRandom) {
+    case 1:
+      carta = "A";
+      break;
+    case 11:
+      carta = "J";
+      break;
+    case 12:
+      carta = "Q";
+      break;
+    case 13:
+      carta = "K";
+      break;
+    default:
+      carta = numRandom;
+  }
 
   document.getElementById("p-top").textContent = simbRandom;
-  document.getElementById("p-center").textContent = numRandom;
+  document.getElementById("p-center").textContent = carta;
   document.getElementById("p-bottom").textContent = simbRandom;
 
   if(simbRandom == "♦" || simbRandom == "♥"){
